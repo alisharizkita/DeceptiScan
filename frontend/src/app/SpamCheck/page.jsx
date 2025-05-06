505e60ffb7af16a9b1b207bc2beb252cfa61b2a0
ryanlukito <ryanlukito13@gmail.com>
2025-05-06 13:46:01 +0700

    making pages responsive (except for article)

--- frontend/src/app/SpamCheck/page.jsx
+++ frontend/src/app/SpamCheck/page.jsx
@@ -7,20 +7,20 @@ import Image from "next/image";
 
 const SpamCheck = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-teal-700 via-white to-white relative">
+    <div className="min-h-screen bg-gradient-to-b from-teal-700 via-white to-white relative sm:px-6 ">
       <Navbar />
       <Checker
         predict_api="predict_spam"
         feedback_api="spam_feedback"
         title="spam"
         resultId="spamresultID"
-      ></Checker>
+      />
       <Image
         src="/SpamCheck.png"
         width={10000}
         height={10000}
         alt="logo"
-        className="w-[468px] h-[490px] absolute right-0 bottom-0"
+        className="hidden md:block w-[468px] h-[490px] absolute right-0 bottom-0"
       />
     </div>
   );
--- frontend/src/components/CheckerResponse.jsx
+++ frontend/src/components/CheckerResponse.jsx
@@ -2,37 +2,38 @@ import React from "react";
 
 const CheckerResponse = ({ result, feedbackSubmitted, handleFeedback }) => {
   return (
-    <div className="w-[55.885vw] h-[12.812vw] flex flex-col items-start justify-center text-black mt-[1vw]">
-      <div className="w-full h-[6.094vw] border border-black rounded-[0.26vw] p-[1vw] shadow-lg">
-        <div className="text-[1.25vw] italic">
+    <div className="w-full max-w-3xl flex flex-col items-start justify-center text-black mt-6">
+      <div className="w-full border border-black rounded-md p-4 shadow-lg">
+        <div className="text-base sm:text-lg italic">
           <h1>
             Your message has been identified as{" "}
             {result.details[0][1] > 0.5
               ? `${parseFloat((result.details[0][1] * 100).toFixed(2))}% ${
                   result.prediction
                 }`
-              : ` ${parseFloat((result.details[0][0] * 100).toFixed(2))}% ${
+              : `${parseFloat((result.details[0][0] * 100).toFixed(2))}% ${
                   result.prediction
                 }`}
-            {`\u00A0`}
-            based on our AI Analysis
+            {"\u00A0"}based on our AI Analysis
           </h1>
         </div>
       </div>
-      <h1 className="text-[1.25vw] mt-[0.5vw]">
+
+      <h1 className="text-base sm:text-lg mt-4">
         Do you think that our DeceptiScan AI gave you the correct answer?
       </h1>
-      <div className="flex space-x-4 mt-4">
+
+      <div className="flex flex-wrap gap-4 mt-4">
         {!feedbackSubmitted ? (
           <>
             <button
-              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 hover:transition ease-in cursor-pointer"
+              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 transition"
               onClick={() => handleFeedback("right")}
             >
               I think the AI is right 👍
             </button>
             <button
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 hover:transition ease-in cursor-pointer"
+              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 transition"
               onClick={() => handleFeedback("wrong")}
             >
               I think the AI is wrong 👎
